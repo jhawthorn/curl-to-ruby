d02da623219ba4790203cf3b9b3e5d8b786832ac
John Hawthorn <john.hawthorn@gmail.com>
2016-05-22 17:48:53 -0700

    Add test suite
    Using this we can test that we generate ruby that makes approximately
    the same requests as curl does.
    
    <3 minitest
    <3 execjs
    <3 webrick (never thought I'd say that)
    <3 ruby

---
+++ test.rb
@@ -0,0 +1,105 @@
+require 'minitest'
+require 'minitest/autorun'
+require 'execjs'
+require 'webrick'
+
+class TestCurlToGo < Minitest::Test
+  JS_CONTEXT = ExecJS.compile(File.read("resources/js/curl-to-ruby.js"))
+
+  def test_simple_get
+    assert_curl_eq "/"
+    assert_curl_eq "/foo.txt"
+  end
+
+  def test_post
+    assert_curl_eq "/", '-X POST -d "foo"'
+    assert_curl_eq "/", '-X POST -d "foobar"'
+  end
+
+  def test_headers
+    assert_curl_eq "/", '-XPOST -H MyCrazyHeader:foo -d "foo"'
+  end
+
+  def test_basic_auth
+    assert_curl_eq "/", "-u banana:coconuts"
+  end
+
+  def test_complex_post
+    assert_curl_eq "/", <<-CURL
+     -X POST \
+     -u API_KEY: \
+     -d 'shipment[to_address][id]=adr_HrBKVA85' \
+     -d 'shipment[from_address][id]=adr_VtuTOj7o' \
+     -d 'shipment[parcel][id]=prcl_WDv2VzHp' \
+     -d 'shipment[is_return]=true' \
+     -d 'shipment[customs_info][id]=cstinfo_bl5sE20Y'
+     CURL
+  end
+
+  def test_simple_data
+    assert_curl_eq "/", "-d foo=bar"
+  end
+
+  def assert_curl_eq(path, curl_args="")
+    curl_req = capture_http do |url|
+      system "curl -s -o /dev/null #{url}#{path} #{curl_args}"
+    end
+
+    ruby_req = capture_http do |url|
+      ruby = curl_to_ruby("curl #{url}#{path} #{curl_args}")
+      eval(ruby)
+    end
+
+    assert_equal curl_req.verb, ruby_req.verb
+    assert_equal curl_req.path, ruby_req.path
+    assert_equal curl_req.body, ruby_req.body
+    assert_equal normalize_headers(curl_req.headers), normalize_headers(ruby_req.headers)
+  end
+
+  Request = Struct.new(:verb, :path, :headers, :body)
+
+  def curl_to_ruby(cmd)
+    JS_CONTEXT.call("curlToRuby", cmd)
+  end
+
+  def normalize_headers(headers)
+    headers = headers.dup
+    headers.delete("accept-encoding")
+    headers.delete("user-agent")
+    headers.delete("host")
+    headers
+  end
+
+  def capture_http
+    server = WEBrick::HTTPServer.new(
+      Port: 0,
+      Logger: WEBrick::Log.new("/dev/null"),
+      AccessLog: []
+    )
+    port = server[:Port]
+    request = nil
+
+    thread = Thread.new do
+      server.mount_proc('/') do |req, res|
+        request = Request.new(
+          req.request_method,
+          req.path,
+          req.header,
+          req.body
+        )
+        res.body = "hello, world"
+        server.shutdown
+      end
+
+      server.start
+
+      request
+    end
+
+    url = "http://127.0.0.1:#{port}"
+    yield url, port
+
+    thread.join
+    thread.value
+  end
+end
